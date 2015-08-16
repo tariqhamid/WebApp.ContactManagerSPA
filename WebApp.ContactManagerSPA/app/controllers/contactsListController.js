c38737b84a9aa2dec0694ab7196e847b318587b0
vasilis <vasiliss_b@hotmail.com>
2015-08-16 15:25:11 +0300

    fix delete contact. remove from list

--- WebApp.ContactManagerSPA/app/controllers/contactsListController.js
+++ WebApp.ContactManagerSPA/app/controllers/contactsListController.js
@@ -1,18 +1,20 @@
 ﻿(function () {
     'use strict';
 
-    app.controller('contactsListController', function ($scope, $location, Contact) {
+    app.controller('contactsListController', function ($scope, $location, Contact, $modal) {
         $scope.contacts = Contact.query();
 
         $scope.remove = function (id, rev) {
             Contact.remove({
                 id: id,
                 rev: rev
+            }).$promise.then(function () {
+                $scope.contacts = Contact.query();
             });
         }
 
         $scope.edit = function (id) {
-            $location.url('/contact' + id);
+            $location.url('/contact/id=' + id);
         }
     });
 }());
